d9c6d0bcae5e3ff235fc1e311e1ba54b937fc0d8
Max Janorschke <max.janorschke@veda.net>
2023-04-19 12:08:08 +0200

    Create contact page #20
    * add mail reference
    * extract elements

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,36 +1,15 @@
 import { SectionContainer } from 'components/Container/SectionContainer';
-import { SocialMediaCard } from 'components/SocialMediaCard/SocialMediaCard';
-import { Grid } from '@mui/material';
-import { SocialMedia } from 'components/SocialMediaCard/social-media.types';
+import { Stack } from '@mui/material';
+import { SocialMediaList } from 'pages/Contact/SocialMediaList';
+import { MailReference } from 'pages/Contact/MailReference';
 
 export const Contact = (): JSX.Element => {
   return (
     <SectionContainer titleTextKey="pages.contact">
-      <Grid container spacing={{ xs: 2, md: 3 }} sx={{ justifyContent: 'center' }}>
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.GIT_HUB} />
-        </Grid>
-
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.LINKED_IN} />
-        </Grid>
-
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.TELEGRAM} />
-        </Grid>
-
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.MAIL} />
-        </Grid>
-
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.INSTAGRAM} />
-        </Grid>
-
-        <Grid item>
-          <SocialMediaCard type={SocialMedia.XING} />
-        </Grid>
-      </Grid>
+      <Stack spacing={2}>
+        <MailReference />
+        <SocialMediaList />
+      </Stack>
     </SectionContainer>
   );
 };

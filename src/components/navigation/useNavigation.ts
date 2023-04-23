47d8dfca7b845c50efb533818a1c0edda713f35a
Max Janorschke <programming@max-janorschke.de>
2023-04-23 20:27:09 +0200

    WIP Footer #28
    * add TODO

--- src/components/navigation/useNavigation.ts
+++ src/components/navigation/useNavigation.ts
@@ -6,11 +6,14 @@ type UseNavigationReturn = {
   navigate: NavigateFunction;
 };
 
+/**
+ * Navigation for the application
+ */
 export const useNavigation = (): UseNavigationReturn => {
   const navigate: NavigateFunction = (anchor: MonoNavigationAnchors) => {
     const elementY = document.getElementById(anchor)?.getBoundingClientRect()?.top || 0;
     const scrollToY = elementY + window.scrollY;
-
+    // TODO use react router if not on the portfolio page
     window.scroll({
       top: scrollToY,
       behavior: 'smooth',

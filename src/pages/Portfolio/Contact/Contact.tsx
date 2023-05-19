4b115808066a23d8c16da52d93a5592b0938fbd8
Max Janorschke <programming@max-janorschke.de>
2023-05-20 00:51:35 +0200

    Show discord integration #118
    * refactoring contacts
    * show discord integration

--- src/pages/Portfolio/Contact/Contact.tsx
+++ src/pages/Portfolio/Contact/Contact.tsx
@@ -1,15 +1,46 @@
 import { SectionContainer } from 'components/Container/SectionContainer';
-import { Stack } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { SocialMediaList } from 'pages/Portfolio/Contact/SocialMediaList';
-import { MailReference } from 'pages/Portfolio/Contact/MailReference';
+import { JSX } from 'react';
+import { T } from 'components/T/T';
+import { SocialMediaCard } from 'components/Card/SocialMediaCard/SocialMediaCard';
+import { SocialMedia } from 'components/Card/SocialMediaCard/social-media.types';
+import { DiscordIntegration } from 'pages/Portfolio/Contact/DiscordIntegration';
 
 export const Contact = (): JSX.Element => {
   return (
     <SectionContainer titleTextKey="pages.contact">
-      <Stack spacing={2}>
-        <MailReference />
-        <SocialMediaList />
-      </Stack>
+      <Grid container sx={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} spacing={3}>
+        <Grid xs={12} item>
+          <SocialMediaList />
+        </Grid>
+
+        <Grid xs={12} item>
+          <Typography variant="h4">
+            <T textKey={'contact.part1'} />
+          </Typography>
+        </Grid>
+
+        <Grid xs={12} item>
+          <Typography variant="h6" sx={{ pb: 1 }}>
+            <T textKey={'contact.part2'} />
+          </Typography>
+
+          <Box display="flex" justifyContent="center">
+            <SocialMediaCard type={SocialMedia.MAIL} linkOnly />
+          </Box>
+        </Grid>
+
+        <Grid xs={12} item>
+          <Typography variant="h6" sx={{ pb: 1 }}>
+            <T textKey={'contact.part3'} />
+          </Typography>
+
+          <Box display="flex" justifyContent="center">
+            <DiscordIntegration width={300} height={300} />
+          </Box>
+        </Grid>
+      </Grid>
     </SectionContainer>
   );
 };

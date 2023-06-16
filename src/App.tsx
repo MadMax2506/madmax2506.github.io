f10460aa5e0a9fa9f322b7a2c4fa17d58fdcdf31
Max Janorschke <max.janorschke@veda.net>
2023-06-16 08:08:08 +0200

    Hide Application in Prod

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { ThemeProvider } from 'context/ThemeContext/ThemeContext';
 import { CookiesProvider } from 'react-cookie';
 import { Routes } from 'routes/Routes';
 import { TranslationProvider } from 'context/TranslationContext/TranslationContext';
+import { ComingSoon } from 'components/ComingSoon';
 
 export const App = () => {
+  // TODO Application is not ready for prod
+  if (import.meta.env.PROD) return <ComingSoon />;
+
   return (
     <CookiesProvider>
       <ThemeProvider>

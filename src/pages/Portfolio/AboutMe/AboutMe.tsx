1b5b47a06c8849a4c09c5afa4e8292d84ba8a0b1
MadMax <programming@max-janorschke.de>
2023-07-09 00:58:14 +0200

    About-Me Description #50
    * about me text

--- src/pages/Portfolio/AboutMe/AboutMe.tsx
+++ src/pages/Portfolio/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
-import { Stack, Typography, useTheme } from '@mui/material';
+import { Stack, useTheme } from '@mui/material';
 import { SectionContainer } from 'components/Container/SectionContainer';
 import { T } from 'components/T/T';
+import dayjs from 'dayjs';
 import { PersonalAvatar } from 'pages/Portfolio/AboutMe/PersonalAvatar';
 import { SkillList } from 'pages/Portfolio/AboutMe/SkillList';
 import './waving-hand.css';
@@ -8,12 +9,15 @@ import './waving-hand.css';
 export const AboutMe = (): JSX.Element => {
   const { breakpoints } = useTheme();
 
+  const yearDiff = Math.floor(dayjs(new Date()).diff('2003-06-25', 'year', true));
+
   return (
     <SectionContainer titleTextKey={'pages.about-me'}>
-      <Stack spacing={4} alignItems="center" sx={{ textAlign: 'center' }}>
+      <Stack spacing={3} alignItems="center" sx={{ textAlign: 'center' }}>
         <PersonalAvatar />
 
-        <Typography
+        <Stack
+          spacing={2}
           sx={{
             width: '100%',
             [breakpoints.up('sm')]: {
@@ -21,8 +25,10 @@ export const AboutMe = (): JSX.Element => {
             },
           }}
         >
-          <T textKey={'about-me'} html />
-        </Typography>
+          <T textKey={'about-me.part1'} html />
+          <T textKey={'about-me.part2'} args={[yearDiff]} />
+          <T textKey={'about-me.part3'} />
+        </Stack>
 
         <SkillList />
       </Stack>

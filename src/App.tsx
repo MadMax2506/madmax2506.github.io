41e4bc07765c1af815bab0092faf7f516bcf24d3
MadMax <programming@max-janorschke.de>
2023-07-21 02:57:40 +0200

    Seobility #36
    * add helmet package
    * add meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { LanguageProvider } from 'context/LanguageContext/LanguageContext';
+import { LanguageProvider, useLanguageContext } from 'context/LanguageContext/LanguageContext';
 import { ThemeProvider } from 'context/ThemeContext/ThemeContext';
 import dayjs from 'dayjs';
 import RelativeTime from 'dayjs/plugin/relativeTime';
 import { CookiesProvider } from 'react-cookie';
+import { Helmet } from 'react-helmet';
 import { Routes } from 'routes/Routes';
 import { queryClient } from 'utils';
 
@@ -16,10 +17,22 @@ export const App = () => {
       <CookiesProvider>
         <LanguageProvider>
           <ThemeProvider>
+            <MetaData />
             <Routes />
           </ThemeProvider>
         </LanguageProvider>
       </CookiesProvider>
     </QueryClientProvider>
+  );
+};
+
+export const MetaData = () => {
+  const { language, translate } = useLanguageContext();
+
+  return (
+    <Helmet htmlAttributes={{ lang: language }}>
+      <title>{translate('meta.title')}</title>
+      <meta name="description" content={translate('meta.description')} />
+    </Helmet>
   );
 };

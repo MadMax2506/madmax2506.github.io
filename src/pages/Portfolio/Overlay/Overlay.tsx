f90dbd6f70eee5acb7644abd9ff2e3f9f28c5e53
Max Janorschke <max.janorschke@veda.net>
2023-08-15 15:42:28 +0200

    feat: :fire: Hide welcome overlay for seven days after the first visit

--- src/pages/Portfolio/Overlay/Overlay.tsx
+++ src/pages/Portfolio/Overlay/Overlay.tsx
@@ -1,5 +1,7 @@
 import { Box, Typography, useTheme } from '@mui/material';
+import dayjs from 'dayjs';
 import { PropsWithChildren, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import './overlay.css';
 
 export const Overlay = ({ children }: PropsWithChildren): JSX.Element => {
@@ -9,13 +11,21 @@ export const Overlay = ({ children }: PropsWithChildren): JSX.Element => {
     },
   } = useTheme();
 
+  const [{ lastVisit }, setCookie] = useCookies<'lastVisit', { lastVisit: string | undefined }>(['lastVisit']);
+  const hideOverlay = !!lastVisit && dayjs().diff(dayjs(lastVisit), 'day') < 1;
+
   const [overlayClass, setOverlayClass] = useState<string | undefined>();
   const [childrenClass, setChildrenClass] = useState<string | undefined>();
 
   setTimeout(() => {
-    setOverlayClass('fade-out');
-    setChildrenClass('fade-in');
+    if (!hideOverlay) {
+      setOverlayClass('fade-out');
+      setChildrenClass('fade-in');
+      setCookie('lastVisit', dayjs().toISOString());
+    }
   }, 1500);
+
+  if (hideOverlay) return <Box>{children}</Box>;
 
   return (
     <>
